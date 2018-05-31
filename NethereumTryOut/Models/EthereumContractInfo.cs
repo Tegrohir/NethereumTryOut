3fbd47a006e0c23e7e66379ccd2ca6651891a49c
Tegrohir <pim.bloot@student.hu.nl>
2018-05-31 10:46:17 +0200

    Initial Commit

---
+++ NethereumTryOut/Models/EthereumContractInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace NethereumTryOut.Models
+{
+    public class EthereumContractInfo : TableEntity
+    {
+        public string Abi { get; set; }
+        public string Bytecode { get; set; }
+        public string TransactionHash { get; set; }
+        public string ContractAddress { get; set; }
+
+        public EthereumContractInfo()
+        {
+
+        }
+
+        public EthereumContractInfo(string name, string abi, string bytecode, string transactionHash)
+        {
+            PartitionKey = "contract";
+            RowKey = name;
+            Abi = abi;
+            Bytecode = bytecode;
+            TransactionHash = transactionHash;
+        }
+    }
+
+}
---
+++ NethereumTryOut/Services/IEthereumService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NethereumTryOut.Models;
+using Nethereum.Contracts;
+
+namespace NethereumTryOut.Services
+{
+    public interface IEthereumService
+    {
+        string AccountAddress { get; set; }
+        Task<bool> SaveContractToTableStorage(EthereumContractInfo contract);
+        Task<EthereumContractInfo> GetContractFromTableStorage(string name);
+        Task<decimal> GetBalance(string address);
+        Task<bool> ReleaseContract(string name, string abi, string byteCode, int gas);
+        Task<string> TryGetContractAddress(string name);
+        Task<Contract> GetContract(string name);
+    }
+}
